21bc2ddae65d29f08a7ad84d74f52bf1e5ac2f82
romanhalaichuk <vi2virbien@gmail.com>
2023-10-29 10:09:42 +0100

    hw3-4

---
+++ src/js/task-04.js
@@ -0,0 +1,16 @@
+let counterValue = 0;
+const valueElement = document.getElementById("value");
+const incrementButton = document.querySelector('[data-action = "increment"]');
+const decrementButton = document.querySelector('[data-action = "decrement"]');
+
+valueElement.textContent = counterValue;
+
+incrementButton.addEventListener("click", () => {
+	counterValue += 1;
+	valueElement.textContent = counterValue;
+});
+
+decrementButton.addEventListener("click", () => {
+	counterValue -= 1;
+	valueElement.textContent = counterValue;
+});
